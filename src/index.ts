5cd4dab162bfc32dbf124971d11738b99d0e89b2
dev3dezital <dev3@dezital.com>
2026-07-14 13:20:48 +0500

    added : commander

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello from TrackItDown");
+#!/usr/bin/env node
+
+import { Command } from "commander";
+
+const program = new Command();
+
+program
+    .name("trackitdown")
+    .description("Create searchable snapshots of your drives")
+    .version("0.1.0");
+
+program
+    .command("hello")
+    .description("Verify that the TrackItDown CLI is working")
+    .argument("[name]", "name to greet", "User")
+    .action((name: string) => {
+        console.log(`Hello ${name}, TrackItDown is ready.`);
+    });
+
+program.parse();
